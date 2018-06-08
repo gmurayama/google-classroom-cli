03615e0efb5a4b6060c93db5d67e1e9eaaf862e2
Gustavo Murayama <gmurayama@outlook.com>
2018-06-08 09:58:38 -0300

    Added: Logger

---
+++ src/GoogleClassroomCli.Infrastructure/Logger/Models/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleClassroom.Infrastructure.Models
+{
+    public class Log
+    {
+        public Log()
+        {
+            Date = DateTime.Now;
+        }
+
+        public LogLevel Level { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string ErrorDetails { get; set; }
+    }
+}
---
+++ src/GoogleClassroomCli.Infrastructure/Logger/Models/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleClassroom.Infrastructure.Models
+{
+    public enum LogLevel
+    {
+        Error,
+        Warning,
+        Information,
+        Debug
+    }
+}
