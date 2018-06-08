31d45cdd4b13ecf06532d6bbdf76596122c399b8
Gustavo Murayama <gmurayama@outlook.com>
2018-06-08 16:34:08 -0300

    Added: Command queue processor

---
+++ src/GoogleClassroomCli.Infrastructure/QueueProcessor/Models/CommandType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleClassroomCli.Infrastructure.Models
+{
+    public enum CommandType
+    {
+        AddCourse,
+        GetCourse,
+        AddStudent
+    }
+}
---
+++ src/GoogleClassroomCli.Infrastructure/QueueProcessor/Models/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleClassroomCli.Infrastructure.Models
+{
+    public class Node
+    {
+        public Node()
+        {
+            Children = new List<Node>();
+        }
+
+        public Node(Command command) : this()
+        {
+            Command = command;
+        }
+
+        public IList<Node> Children { get; private set; }
+        public Command Command { get; set; }
+    }
+}
