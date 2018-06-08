529977cf8cc64729a5b1b39f65a21f6b942191c4
Gustavo Murayama <gmurayama@outlook.com>
2018-06-08 09:58:16 -0300

    Added: Services to communicate to Google Classroom API

---
+++ src/GoogleClassroomCli.Infrastructure/GoogleClassroom/CourseService.cs
@@ -0,0 +1,97 @@
+﻿using GoogleClassroomCli.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleClassroomCli.Infrastructure.GoogleClassroom
+{
+    public class CourseService : GoogleClassroomService
+    {
+        public CourseService(string privateKeyPath) : base(privateKeyPath) { }
+
+        public CourseService(string privateKeyPath, string emailToImpersonate) : base(privateKeyPath, emailToImpersonate) { }
+
+        public CourseService(string privateKeyPath, string emailToImpersonate, params string[] scopes) : base(privateKeyPath, emailToImpersonate, scopes) { }
+
+        public Course AddCourse(Course course)
+        {
+            var courseClassroom = new Google.Apis.Classroom.v1.Data.Course
+            {
+                Name = course.Name,
+                Section = course.Section,
+                Room = course.Room,
+                OwnerId = course.PrimaryTeacher.Id ?? course.PrimaryTeacher.Email
+            };
+
+            var courseAddedToClassroom = Service.Courses.Create(courseClassroom).Execute();
+
+            return CourseClassroomObjectToCourseDomainObject(courseAddedToClassroom);
+        }
+
+        public Course FindCourseById(string courseId)
+        {
+            var courseFromClassroom = Service.Courses.Get(courseId).Execute();
+
+            return CourseClassroomObjectToCourseDomainObject(courseFromClassroom);
+        }
+
+        public Student AddStudentToCourse(string courseId, string studentId)
+        {
+            var studentClassroom = new Google.Apis.Classroom.v1.Data.Student
+            {
+                CourseId = courseId,
+                UserId = studentId
+            };
+
+            var studentFromClassroom = Service.Courses.Students.Create(studentClassroom, courseId).Execute();
+
+            return new Student
+            {
+                Id = studentFromClassroom.UserId,
+                Email = studentFromClassroom.Profile.EmailAddress,
+                Name = studentFromClassroom.Profile.Name.FullName
+            };
+        }
+
+        public void RemoveStudentFromCourse(string courseId, string studentId)
+        {
+            Service.Courses.Students.Delete(courseId, studentId).Execute();
+        }
+
+        public Teacher GetTeacherProfile(string teacherId)
+        {
+            var teacherFromClassroom = Service.UserProfiles.Get(teacherId).Execute();
+
+            return new Teacher
+            {
+                Id = teacherFromClassroom.Id,
+                Email = teacherFromClassroom.EmailAddress,
+                Name = teacherFromClassroom.Name.FullName
+            };
+        }
+
+        public Student GetStudentProfile(string studentId)
+        {
+            var studentFromClassroom = Service.UserProfiles.Get(studentId).Execute();
+
+            return new Student
+            {
+                Id = studentFromClassroom.Id,
+                Email = studentFromClassroom.EmailAddress,
+                Name = studentFromClassroom.Name.FullName
+            };
+        }
+
+        private Course CourseClassroomObjectToCourseDomainObject(Google.Apis.Classroom.v1.Data.Course courseFromClassroom)
+        {
+            return new Course
+            {
+                Id = courseFromClassroom.Id,
+                Name = courseFromClassroom.Name,
+                Room = courseFromClassroom.Room,
+                Section = courseFromClassroom.Section,
+                PrimaryTeacher = new Teacher { Id = courseFromClassroom.OwnerId }
+            };
+        }
+    }
+}
---
+++ src/GoogleClassroomCli.Infrastructure/GoogleClassroom/GoogleClassroomService.cs
@@ -0,0 +1,79 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Classroom.v1;
+using Google.Apis.Services;
+using System.IO;
+
+namespace GoogleClassroomCli.Infrastructure.GoogleClassroom
+{
+    public abstract class GoogleClassroomService
+    {
+        private GoogleCredential _credential;
+        private string _credentialEmail;
+        private string[] _scopes;
+        private ClassroomService _classroomService;
+
+        public string CredentialEmail => _credentialEmail;
+
+        protected ClassroomService Service
+        {
+            get
+            {
+                if (_classroomService == null)
+                {
+                    InitializeClassroomService();
+                    return _classroomService;
+                }
+                else
+                    return _classroomService;
+            }
+        }
+
+        public GoogleClassroomService(string privateKeyPath)
+        {
+            GenerateCredentialFromPrivateKey(privateKeyPath);
+        }
+
+        public GoogleClassroomService(string privateKeyPath, string emailToImpersonate) : this(privateKeyPath)
+        {
+            ImpersonateEmail(emailToImpersonate);
+        }
+
+        public GoogleClassroomService(string privateKeyPath, string emailToImpersonate, params string[] scopes) : this(privateKeyPath, emailToImpersonate)
+        {
+            SetScopes(scopes);
+        }
+
+        private void GenerateCredentialFromPrivateKey(string privateKeyPath)
+        {
+            using (var stream = new FileStream(@privateKeyPath, FileMode.Open, FileAccess.Read))
+            {
+                _credential = GoogleCredential.FromStream(stream);
+            }
+        }
+
+        public void ImpersonateEmail(string email, params string[] scopes)
+        {
+            _credential = _credential.CreateWithUser(email);
+            _credentialEmail = email;
+
+            if (scopes?.Length != 0)
+                _scopes = scopes;
+
+            InitializeClassroomService();
+        }
+
+        public void SetScopes(params string[] scopes)
+        {
+            _scopes = scopes;
+            InitializeClassroomService();
+        }
+
+        private void InitializeClassroomService()
+        {
+            _classroomService = new ClassroomService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credential.CreateScoped(_scopes)
+            });
+        }
+    }
+}
